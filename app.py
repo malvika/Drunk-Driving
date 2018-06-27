4f641cb385bae373b0f5e0fd184ad1b6d5a652e0
matiullah23 <matiullah.hasher@gmail.com>
2018-06-27 13:29:55 -0400

    updated app.py

--- app.py
+++ app.py
@@ -1,38 +1,113 @@
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
-import os
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect)
 
-from flask import (Flask, jsonify, render_template, request, redirect)
+from flask_sqlalchemy import SQLAlchemy
 
+#################################################
 # Flask Setup
+#################################################
 app = Flask(__name__)
 
+#################################################
 # Database Setup
-dbfile = os.path.join('Data Set/drinkingdriving.sqlite')
-engine = create_engine(f"sqlite:///{dbfile}")
+#################################################
 
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(engine, reflect=True)
+# The database URI
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///Dataset/drinkingdriving.sqlite"
 
-Crashes = Base.classes.CrashesDC
-Police = Base.classes.PoliceperCapita
-Sundays = Base.classes.Sundays
+db = SQLAlchemy(app)
 
-session = Session(engine)
+
+class Danger(db.Model):
+    __tablename__ = 'CrashesDC'
+
+    LATITUDE = db.Column(db.Text, primary_key=True)
+    LONGITUDE = db.Column(db.Text)
+    REPORT_DATE = db.Column(db.Text)
+    REPORT_TIME = db.Column(db.Text)
+    DRIVERS_IMPAIRED = db.Column(db.Text)
+
+    def __repr__(self):
+        return '<CrashesDC %r>' % (self.name)
+
+class Sunday(db.Model):
+    __tablename__ = 'Sundays'
+
+    SundayLaw = db.Column(db.Text, primary_key=True)
+    Fatality = db.Column(db.Text)
+    DUI = db.Column(db.Text)
+
+    def __repr__(self):
+        return '<Sundays %r>' % (self.name)
+
+
+class LawEnforcement(db.Model):
+    __tablename__ = 'PoliceperCapita'
+    Year = db.Column(db.Text)
+    State = db.Column(db.Text, primary_key=True)
+    Police = db.Column(db.Text)
+    Fatalites = db.Column(db.Text)
+
+    def __repr__(self):
+        return '<PoliceperCapita %r>' % (self.name)
+
+#################################################
+# Flask Routes
+#################################################
+
 
 @app.route("/")
-def index():
-    return render_template('index.html')
+def home():
+    """Render Home Page."""
+    return render_template("index.html")
+
+
+@app.route("/crash")
+def crash_data():
+    """test"""
+
+    # query for the top 10 emoji data
+    results = db.session.query(Danger.DRIVERS_IMPAIRED).\
+        order_by(Danger.DRIVERS_IMPAIRED.desc()).\
+        limit(10).all()
+
+    # Select the top 10 query results
+    emoji_char = [result[0] for result in results]
+
+    return jsonify(emoji_char)
 
 @app.route("/police")
-def policeroute():
-    return render_template('index.html')
+def police_data():
+    """test"""
+
+    # query for the top 10 emoji data
+    results = db.session.query(LawEnforcement.State).\
+        order_by(LawEnforcement.State.desc()).\
+        limit(10).all()
+
+    # Select the top 10 query results
+    results = [result[0] for result in results]
+
+    return jsonify(results)
 
 
+@app.route("/sunday")
+def sunday_data():
+    """test"""
 
-if __name__ == "__main__":
-        app.run()
+    # query for the top 10 emoji data
+    results = db.session.query(Sunday.Fatality).\
+        order_by(Sunday.Fatality.desc()).\
+        limit(10).all()
+
+    # Select the top 10 query results
+    results = [result[0] for result in results]
+
+    return jsonify(results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
